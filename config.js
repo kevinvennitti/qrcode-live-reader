b3755d44e06c439c6cdd1833316e7ec7010b0922
Kévin VENNITTI <kevin.vennitti@gmail.com>
2024-07-31 15:51:16 +0200

    Audio version: audio name is code to scan

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+  // QR code colors: 
+  // - black-on-white (dark QR code on bright background)
+  // - white-on-black (bright QR code on dark background)
+  // - both
+  theme: 'black-on-white',
+
+  // Preferred camera to use
+  // - environment: back camera
+  // - user: front camera
+  camera: 'environment',
+
+  // Highlight scan region on video
+  // highlightScanRegion: false,
+
+  // Outline QR code edges on video 
+  // highlightQRCodeOutline: false,
+
+};
